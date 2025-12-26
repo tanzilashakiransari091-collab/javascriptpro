6d1c5a377fda8cc1e249e4aef58d9b57c5f16a82
Tanzila shakir <tanzilashakiransari091@gmail.com>
2025-12-26 14:38:29 +0500

    Implement basic arithmetic operations and variable examples
    Added arithmetic operations and variable manipulations to app.js.

--- app.js
+++ app.js
@@ -21,6 +21,74 @@ var $price;
 var _Digit;
 var username;
 var Fullname;
+
+
+
+var num1 = 10
+var num2 = 20
+var num3 = num1+num2
+console.log(num3)
+document.write("addition of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
+
+var num1 = 20
+var num2 = 10
+var num3 = num1-num2
+console.log(num3)
+document.write("subtraction of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
+
+var num1 = 10
+var num2 = 20
+var num3 = num1*num2
+console.log(num3)
+document.write("multiplication of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
+
+var num1 = 10
+var num2 = 20
+var num3 = num1/num2
+console.log(num3)
+document.write("division of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
+
+var num1 = 10
+var num2 = 20
+var num3 = num1%num2
+console.log(num3)
+document.write("remainder of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
+
+//TASK 2//
+var num
+document.write("value after decleartion is undefined<br><br>")
+
+var num = 10
+document.write("initial value:" + num + "<br><br>")
+
+var num = 5
+num++
+console.log(num)
+document.write(" Value after increement is " + num + "<br><br>")
+var num = num + 7
+document.write(" value after addition is " + num + "<br><br>")
+
+num--
+document.write(" value after decreement is " + num + "<br><br>")
+
+
+var remainder = num % 3;
+document.write("The remainder is: " + remainder + "<br><br>");
+
+
+var ticketPrice = 600
+var totalcost = ticketPrice * 5
+console.log(totalcost)
+document.write(" Total cost to buy 5 tickets is " + totalcost + "<br><br>");
+
+
+var tablenum = 4
+document.write(" table of " + tablenum + " :<br><br> ")
+
+//var mynum = num * 1
+//console.log(mynum)//
+//document.write(" table of " + tablenum + " :<br><br> ")
+//document.write("mynum")//
 var num1;
 
 //illegal variable
