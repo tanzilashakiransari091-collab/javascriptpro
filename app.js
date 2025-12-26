6150b90e6b4fd4b75d43bc23398628507d06bcc7
Tanzila shakir <tanzilashakiransari091@gmail.com>
2025-12-26 14:44:35 +0500

    Fix variable name and clean up app.js
    Updated variable name from 'anays' to 'anaya' and removed unused code.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ alert(birth)
 
 //chapter:4
 //q1
-var ab, bc, cd ="my name is anays"
+var ab, bc, cd ="my name is anaya"
 
 //q2
 // legal variable
@@ -21,100 +21,6 @@ var $price;
 var _Digit;
 var username;
 var Fullname;
-
-
-
-var num1 = 10
-var num2 = 20
-var num3 = num1+num2
-console.log(num3)
-document.write("addition of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
-
-var num1 = 20
-var num2 = 10
-var num3 = num1-num2
-console.log(num3)
-document.write("subtraction of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
-
-var num1 = 10
-var num2 = 20
-var num3 = num1*num2
-console.log(num3)
-document.write("multiplication of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
-
-var num1 = 10
-var num2 = 20
-var num3 = num1/num2
-console.log(num3)
-document.write("division of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
-
-var num1 = 10
-var num2 = 20
-var num3 = num1%num2
-console.log(num3)
-document.write("remainder of " + num1 + " and " + num2 + " is " + num3 + "<br><br>")
-
-//TASK 2//
-var num
-document.write("value after decleartion is undefined<br><br>")
-
-var num = 10
-document.write("initial value:" + num + "<br><br>")
-
-var num = 5
-num++
-console.log(num)
-document.write(" Value after increement is " + num + "<br><br>")
-var num = num + 7
-document.write(" value after addition is " + num + "<br><br>")
-
-num--
-document.write(" value after decreement is " + num + "<br><br>")
-
-
-var remainder = num % 3;
-document.write("The remainder is: " + remainder + "<br><br>");
-
-
-var ticketPrice = 600
-var totalcost = ticketPrice * 5
-console.log(totalcost)
-document.write(" Total cost to buy 5 tickets is " + totalcost + "<br><br>");
-
-
-var tablenum = 4
-document.write(" table of " + tablenum + " :<br><br> ")
-
-//var mynum = num * 1
-//console.log(mynum)//
-//document.write(" table of " + tablenum + " :<br><br> ")
-//document.write("mynum")//
-var num1;
-
-//illegal variable
-//var 1FullName;
-//var @name;
-// var var
-//var my-variable 
-
-//Q3
-var head = "<h2>Hello World</h2>";
-document.write(head);
-
- //var contain this only letter, digit,$ _
-var $dollar;
-var _underscore;
-var  name;
-var digit;
-
-//variale must begin $ dollar and _ underscore
-var $price =50 
-var _name ="ALISHBA"
-
-//Varibale are Case  Sensitve #example
-//var var =
-
-//var name should notbe case reserved example'var'
 
 
 
