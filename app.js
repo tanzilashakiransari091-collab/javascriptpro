03e9735d590d9f8867a3d482aa711e705c78c503
tanzilashakiransari091 <tanzilashakiransari091@gmail.com>
2025-12-26 14:16:48 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+//chapter:3
+
+var frontend = "I am a frontend dveloper";
+alert(frontend)
+
+var visit = "you can visited my website";
+alert(visit)
+
+var birth = "my birth year is 2003";
+alert(birth)
+
+
+
+//chapter:4
+//q1
+var ab, bc, cd ="my name is anays"
+
+//q2
+// legal variable
+var $price;
+var _Digit;
+var username;
+var Fullname;
+var num1;
+
+//illegal variable
+//var 1FullName;
+//var @name;
+// var var
+//var my-variable 
+
+//Q3
+var head = "<h2>Hello World</h2>";
+document.write(head);
+
+ //var contain this only letter, digit,$ _
+var $dollar;
+var _underscore;
+var  name;
+var digit;
+
+//variale must begin $ dollar and _ underscore
+var $price =50 
+var _name ="ALISHBA"
+
+//Varibale are Case  Sensitve #example
+//var var =
+
+//var name should notbe case reserved example'var'
+
+
+
