6e08712b602373dfbafd5a720beaa1dd29d6b2a7
Tanzila shakir <tanzilashakiransari091@gmail.com>
2025-12-29 11:57:30 +0500

    Add JavaScript variable examples and arithmetic operations

--- app.js
+++ app.js
@@ -49,4 +49,133 @@ var _name ="ALISHBA"
 //var name should notbe case reserved example'var'
 
 
+/chapter:4
+//q1
+var ab, bc, cd ="my name is anays"
+
+//q2
+// legal variable
+var $price;
+var _Digit;
+var username;
+var Fullname;
+var num1;
+
+//illegal variable
+//var 1FullName;
+//var @name;
+// var var
+//var my-variable 
+
+//Q3
+var head = "<h2>Hello World</h2>";
+document.write(head);
+
+ //var contain this only letter, digit,$ _
+var $dollar;
+var _underscore;
+var  name;
+var digit;
+
+//variale must begin $ dollar and _ underscore
+var $price =50 
+var _name ="ALISHBA"
+
+//Varibale are Case  Sensitve #example
+//var var =
+
+//var name should notbe case reserved example'var'
+
+
+
+
+// chapter #5
+//Q1
+var num1 = 3;
+var num2 = 5;
+var result = num1 + num2;
+
+console.log(result);
+
+
+document.write("<h2>Addition Result</h2>");
+document.write("<p>Addition of " + num1 + " + " + num2 + " is " + result + "</p>");
+
+
+//Q2
+//subtraction
+var num1 = 3;
+var num2 = 5;
+ result = num1 - num2;
+
+console.log(result);
+
+
+document.write("<h2>Subtraction Result</h2>");
+document.write("<p>subtraction of " + num1 + " - " + num2 + " is " + result + "</p>");
+
+//Multiply
+var num1 = 3;
+var num2 = 5;
+result = num1 * num2;
+
+console.log(result);
+
+
+document.write("<h2>Multiply Result</h2>");
+document.write("<p>Multiply of " + num1 + " * " + num2 + " is " + result + "</p>");
+
+//Divide
+var num1 = 3;
+var num2 = 5;
+ result = num1 / num2;
+
+console.log(result);
+
+
+document.write("<h2>Divide Result</h2>");
+document.write("<p>Divide of " + num1 + " / " + num2 + " is " + result + "</p>");
+
+// modulus
+var num1 = 3;
+var num2 = 5;
+ result = num1 % num2;
+
+console.log(result);
+
+
+document.write("<h2>Modulus Result</h2>");
+document.write("<p>Modulus of " + num1 + " % " + num2 + " is " + result + "</p>");
+
+//Q3
+//a
+var num ="5"
+document.write("DEclare value is defined 5")
+
+//b
+
+var num
+document.write("Num value is undefined in browser")
+
+//d c 
+var num = 5
+document.write("initial value:" + num  )
+
+
+
+//table of 4 
+var num = 4
+document.write("<h1> Table of " + num + " </h1> ")
+document.write(num + " x 1 = " + 4 + " <br> ")
+document.write(num + " x 2 = " + 8 + " <br> ")
+document.write(num + " x 3 = " + 12 + " <br> ")
+document.write(num + " x 4 = " + 16 + " <br> ")
+document.write(num + " x 5 = " + 20 + " <br> ")
+document.write(num + " x 6 = " + 24 + " <br ") 
+document.write(num + " x 7 = " + 28 + " <br> ")
+document.write(num + " x 8 = " + 32 + " <br> ")
+document.write(num + " x 9 = " + 36 + " <br> ")
+document.write(num + " x 10 = " + 40 + " <br> ")
+
+
 
