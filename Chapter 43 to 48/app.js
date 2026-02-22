c0b7ca13df9f514c795fb209e8f7ed283a5318c3
Tanzila shakir <tanzilashakiransari091@gmail.com>
2026-02-23 01:16:19 +0500

    Add files via upload

---
+++ Chapter 43 to 48/app.js
@@ -0,0 +1,26 @@
+ function greet (){
+var msg = "Welcome to JS World"
+alert (msg)  
+
+}
+ 
+
+function mobileClick (e){
+    alert( "this is good mobile")
+}
+
+
+function flowerClickOn(e)  {
+    e.src ="https://i.pinimg.com/originals/3e/f8/1c/3ef81c2417123c556303898dd804cc4e.gif"
+
+
+
+}  
+
+
+function flowerClickOf(e)  {
+    e.src ="images/Screenshot 2026-02-23 005243.png"
+
+
+
+} 
